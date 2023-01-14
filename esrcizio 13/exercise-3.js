45b9cc8c518af2313f304d56bc0a942dabcac340
Nelexiad <117077787+Nelexiad@users.noreply.github.com>
2023-01-14 21:05:26 +0100

    Add files via upload

---
+++ esrcizio 13/exercise-3.js
@@ -0,0 +1,25 @@
+
+const initialValue= 0
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+function calculateAverageAge(persons) {
+  let totalAge = persons.reduce((accumulator, peopleage) => accumulator + peopleage.age, initialValue);
+  let averAge = Math.round(totalAge / persons.length);
+  return averAge
+  
+}
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
