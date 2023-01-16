62094bcbf0d4ea8526dd1e5dd49c1d1d74931606
Nelexiad <117077787+Nelexiad@users.noreply.github.com>
2023-01-16 18:34:10 +0100

    Add files via upload

---
+++ esercizio 23/exercise-3.js
@@ -0,0 +1,14 @@
+function isAdult(person) {
+  return age >= 18;
+}
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+({ id, firstName, lastName, age } = person);
+
+console.log(isAdult(person));
