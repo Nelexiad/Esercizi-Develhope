3cea2297b807ae50d154ac710113d0b59eed7b8b
Nelexiad <117077787+Nelexiad@users.noreply.github.com>
2023-02-10 20:01:11 +0100

    Add files via upload

---
+++ src18/Welcome.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Age from './Age';
+
+
+class Welcome extends React.Component{
+
+    static defaultProps = {
+        name: 'Dan',
+    };
+
+
+    render() {
+        return (
+            <div>
+                <p>Welcome {this.props.name}</p>
+                {  this.props.age >= 18
+                    && this.props.age < 65
+                    && this.props.name === 'John'
+                    && <Age age={this.props.age} />}
+                
+           </div>
+    )
+}
+}
+export default Welcome
