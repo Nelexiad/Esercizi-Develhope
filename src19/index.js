5ad1af45f26e85ade8d881c7f7787766c69796df
Nelexiad <117077787+Nelexiad@users.noreply.github.com>
2023-02-10 20:03:35 +0100

    Add files via upload

---
+++ src19/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './esercizio8/App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+      <App />
+);
