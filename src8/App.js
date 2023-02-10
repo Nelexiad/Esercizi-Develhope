ae067ab019b1be3f632b3eb58a17a1be5c084d12
Nelexiad <117077787+Nelexiad@users.noreply.github.com>
2023-02-10 19:08:43 +0100

    Add files via upload

---
+++ src8/App.js
@@ -0,0 +1,16 @@
+lines (12 sloc)  239 Bytes
+
+import React from "react";
+import Hello from './Hello';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Hello />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src8/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Hello extends React.Component{
+    render() {
+        return (
+            <div>
+            <h1>Hello,World</h1>
+                <p>what a beatiful day</p>
+                </div>
+        )
+    }
+}
+export default Hello
