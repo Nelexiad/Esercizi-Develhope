9e41d31eec649a7a68f2ab6448036f94c356007b
Nelexiad <117077787+Nelexiad@users.noreply.github.com>
2023-02-10 23:30:24 +0100

    Add files via upload

---
+++ src28/Login.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+class Login extends React.Component {
+    state = {
+      username: "",
+      password: "",
+      remember: false
+    };
+  
+    handleInputChange = event => {
+      const { name, value, type, checked } = event.target;
+      type === "checkbox"
+        ? this.setState({ [name]: checked })
+        : this.setState({ [name]: value });
+  };
+  handleLogin = () => {
+    this.props.onLogin(this.state);
+  };
+  
+    render() {
+      return (
+        <form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="remember">Remember me:</label>
+            <input
+              type="checkbox"
+              id="remember"
+              name="remember"
+              checked={this.state.remember}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>{ this.state.username}</div>
+          <div>{ this.state.password}</div>
+          <div>{String(this.state.remember)}</div>
+          <button disabled={!this.state.username || !this.state.password} onClick={this.handleLogin}>
+          Login
+        </button>
+        </form>
+      );
+  }
+  
+}
+
+export default Login
