01e436a4ab1c785929b1be001f69e6f3407c2b9f
Daniele Ugo D'Anna <117077787+Nelexiad@users.noreply.github.com>
2023-02-18 17:33:27 +0100

    Add files via upload

---
+++ src34/TodoList.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class TodoList extends React.Component {
+  state = {
+    items: ["item1", "item2", "item3"],
+    value: "",
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState((prevState) => ({
+      items: [...prevState.items, prevState.value],
+      value: "",
+    }));
+  };
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+  handleReset = () => {
+    this.setState({ items: [], value: "" });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            autoFocus
+          />
+          <button type="submit">add</button>
+          <button onClick={this.handleReset}>Reset</button>
+        </form>
+        <ul>
+          {this.state.items.map((items, index) => {
+            return <li key={items[index]}>{items}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
