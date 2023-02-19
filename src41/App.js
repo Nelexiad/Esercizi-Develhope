3552201d0b570f565e7571efc59b9e1a10d9ac2b
Daniele Ugo D'Anna <117077787+Nelexiad@users.noreply.github.com>
2023-02-19 17:31:51 +0100

    Add files via upload

---
+++ src41/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DisplayLanguage from "./DisplayLanguage";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <DisplayLanguage />
+      </div>
+    );
+  }
+}
+
+export default App;
