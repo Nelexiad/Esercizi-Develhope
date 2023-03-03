4ce751787bfae44508f06ec089d16e9be635aa61
Daniele Ugo D'Anna <117077787+Nelexiad@users.noreply.github.com>
2023-03-03 18:52:55 +0100

    Delete src9 directory

--- src9/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './esercizio8/App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-      <App />
-);
