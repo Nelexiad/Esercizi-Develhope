4d24fa6bf27ddc5337fa656fb4e1c6999c5cbdbf
Daniele Ugo D'Anna <117077787+Nelexiad@users.noreply.github.com>
2023-03-12 15:27:57 +0100

    Add files via upload

---
+++ src57/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SelectLanguage from "./SelectLanguage";
+
+class App extends React.Component {
+  render() {
+    return <SelectLanguage />;
+  }
+}
+
+export default App;
