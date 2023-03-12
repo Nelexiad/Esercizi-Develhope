21fcdda8af30f481d7f99e99fdf2bb2be6f8be0b
Daniele Ugo D'Anna <117077787+Nelexiad@users.noreply.github.com>
2023-03-12 15:42:39 +0100

    Add files via upload

---
+++ src60/App.js
@@ -0,0 +1,12 @@
+import Welcome from "./Welcome";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Welcome name='Alessandro' />} />
+    </Routes>
+  );
+};
+
+export default App;
