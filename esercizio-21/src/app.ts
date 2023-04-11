5a36d421bcb238503122601a1beb1af1e2cb131a
Nelexiad <daniele.danna96@gmail.com>
2023-04-11 16:23:52 +0200

    passport session

--- esercizio-21/src/app.ts
+++ esercizio-21/src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import "express-async-errors";
-
+import { passport } from "./lib/middleware/passport";
 import { initCorsMiddleware } from "./lib/middleware/cors";
 
 import { initMulterMiddleware } from "./lib/middleware/multer";
 
+import { initSessionMiddleware } from "./lib/middleware/session";
 const upload = initMulterMiddleware();
 
 import {
@@ -14,15 +15,15 @@ import {
   validationErrorMiddleware,
 } from "./lib/validation";
 
-
-
 const app = express();
 
-app.use(initCorsMiddleware())
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(initCorsMiddleware());
 
 app.use(express.json());
-
-
 
 app.use(validationErrorMiddleware);
 
--- esercizio-21/src/config.ts
+++ esercizio-21/src/config.ts
@@ -1,7 +1,8 @@
 const missingSetting = "missing value";
 
 const config = {
-    "PORT": process.env.PORT || missingSetting,
-}
+  PORT: process.env.PORT || missingSetting,
+  SESSION_SECRET: process.env.SESSION_SECRET || missingSetting,
+};
 
 export default config;
