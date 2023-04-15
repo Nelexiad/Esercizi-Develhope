88122f60b5b6b24f4b388281d1eaf9a99719e93a
Nelexiad <daniele.danna96@gmail.com>
2023-04-15 16:35:13 +0200

    esercizio-22

---
+++ esercizio-22/counter.mjs
@@ -0,0 +1,15 @@
+class Counter {
+  constructor() {
+    this.count = 0;
+  }
+  increment() {
+    this.count++;
+  }
+}
+
+export const counterInstance = {
+  count: 0,
+  increment() {
+    this.count++;
+  },
+};
---
+++ esercizio-22/script-2.mjs
@@ -0,0 +1,5 @@
+import "./script-1.mjs";
+
+import { counterInstance } from "./counter.mjs";
+
+counterInstance.increment();
